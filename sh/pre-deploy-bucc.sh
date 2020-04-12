44d8e790687d078e48b89ce569137dffea7dcb5b
Sean Noyes <sean.noyes@gmail.com>
2020-04-12 03:57:32 -0500

    Update pre-deploy-bucc.sh

--- sh/pre-deploy-bucc.sh
+++ sh/pre-deploy-bucc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt update
 sudo apt upgrade -y
-sudo apt install direnv jq software-properties-common -y
+sudo apt install direnv jq software-properties-common build-essential -y
 sudo add-apt-repository ppa:certbot/certbot -y
 sudo apt update
 sudo apt install certbot python3-certbot-dns-cloudflare -y
